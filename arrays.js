c2a6f9128cfd3dd98bc8cf121d4c4c6c2897942f
Marie Filbey <patriciamariefilbey@gmail.com>
2017-02-01 15:32:09 -0500

    Finished all homework.

--- arrays.js
+++ arrays.js
@@ -102,8 +102,8 @@ function findIs(strings) {
   var containsIs = [];
   for (var i = 0; i < strings.length; i++) {
     if(strings[i].includes ("is")) {
-      containsIs.push(strings[i].includes);
-      /*console.log(strings[i]);*/
+      containsIs.push(strings[i]); //needed to remove the .includes because that is a function that does NOT need to be pushed
+      console.log(strings[i]);
     }
   } return containsIs;
 }
@@ -134,3 +134,22 @@ function joinArrays (numbers, strings) {
 // 6. Use the Instructors array and find all that teach JavaScript,
 //    then sort them alphabetically
 // ---------------------------
+var instructors = [
+    { firstname : 'JD', teaches : 'JavaScript'},
+    { firstname : 'Tim', teaches : 'JavaScript'},
+    { firstname : 'Brit', teaches : 'Ruby'},
+    { firstname : 'Joe', teaches : 'iOS'},
+    { firstname : 'Jake', teaches : 'JavaScript'},
+    { firstname : 'Will', teaches : 'JavaScript'},
+    { firstname : 'Calvin', teaches : 'JavaScript'},
+    { firstname : 'James', teaches : 'Ruby'}
+];
+var trueJS = [];
+function teachesJS (instructors){
+
+  for (var i =0; i < instructors.length; i++) {
+    if (instructors[i].teaches === 'JavaScript') {
+      trueJS.push(instructors[i].firstname);
+    }
+  } return trueJS.sort();
+}
